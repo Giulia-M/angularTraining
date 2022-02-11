a42d26398d6291e419265780d0d3628e9c5d6a71
Giulia-M <giuliamaggio16@gmail.com>
2022-02-11 13:04:02 +0100

    http communication

--- workshop/demo1/src/app/app.component.ts
+++ workshop/demo1/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from './model/user';
@@ -101,19 +102,38 @@ import { User } from './model/user';
   ],
 })
 export class AppComponent {
-  users: User[] = [
-    // list of users
-    { id: 1, label: 'Fabio', gender: 'M', age: 20 },
-    { id: 2, label: 'Lorenzo', gender: 'M', age: 37 },
-    { id: 3, label: 'Silvia', gender: 'F', age: 70 },
-  ];
+  // users: User[] = [
+  //   // list of users
+  //   { id: 1, label: 'Fabio', gender: 'M', age: 20 },
+  //   { id: 2, label: 'Lorenzo', gender: 'M', age: 37 },
+  //   { id: 3, label: 'Silvia', gender: 'F', age: 70 },
+  // ];
+
+  users: User[];
+  URL = 'http://localhost:3000';
+  constructor(private http: HttpClient) {
+    this.init();
+  }
+  init() {
+    this.http.get<User[]>(this.URL + '/users').subscribe((res) => {
+      this.users = res;
+    });
+  }
+
   deleteHandler(userToRemove: User) {
-    this.users = this.users.filter((u) => u.id !== userToRemove.id);
+    this.http.delete(`${this.URL}/users/${userToRemove.id}`).subscribe(() => {
+      this.users = this.users.filter((u) => u.id !== userToRemove.id);
+    });
   }
   saveHandler(f: NgForm) {
     const user = f.value as User;
-    user.id = Date.now(); // add a fake ID
-    this.users = [...this.users, user];
-    f.reset();
+
+    this.http.post<User>(`${this.URL}/users/`, user).subscribe((dbUser) => {
+      this.users = [...this.users, dbUser];
+      f.reset();
+    });
+    // add a fake ID
+    // user.id = Date.now();
+    // this.users = [...this.users, user];
   }
 }
