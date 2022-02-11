b9acdc817b3b813053682ed18ffdef136347509c
Giulia-M <giuliamaggio16@gmail.com>
2022-02-11 12:24:58 +0100

    template driven form

--- workshop/demo1/src/app/app.component.ts
+++ workshop/demo1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { User } from './model/user';
 
 @Component({
@@ -6,25 +7,55 @@ import { User } from './model/user';
   template: `
     <!-- <h4>Users</h4>
     <pre>{{ users | json }}</pre> -->
+
     <div class="container">
-      <h4>Users</h4>
-      <div
-        *ngFor="let u of users"
-        class="list-group-item"
-        [ngClass]="{
-          male: u.gender === 'M',
-          female: u.gender === 'F'
-        }"
-      >
-        <i
-          class="fa fa-3x"
+      <form class="card card-body mt-3" #f="ngForm" (submit)="saveHandler(f)">
+        <input
+          type="text"
+          [ngModel]
+          name="label"
+          placeholder="Add user name"
+          class="form-control"
+          required
+        />
+
+        <select [ngModel] name="gender" class="form-control" required>
+          <option [ngValue]="null">Select option</option>
+          <option value="M">M</option>
+          <option value="F">F</option>
+        </select>
+
+        <button class="btn" [disabled]="f.invalid">Save</button>
+      </form>
+
+      <hr />
+
+      <hr />
+      <!--  -->
+
+      <div class="container">
+        <h4>Users</h4>
+        <div
+          *ngFor="let u of users"
+          class="list-group-item"
           [ngClass]="{
-            'fa-mars male': u.gender === 'M',
-            'fa-venus female': u.gender === 'F'
+            male: u.gender === 'M',
+            female: u.gender === 'F'
           }"
-        ></i>
-        {{ u.label }}
-        <i class="fa fa-trash fa-2x pull-right" (click)="deleteHandler(u)"></i>
+        >
+          <i
+            class="fa fa-3x"
+            [ngClass]="{
+              'fa-mars male': u.gender === 'M',
+              'fa-venus female': u.gender === 'F'
+            }"
+          ></i>
+          {{ u.label }}
+          <i
+            class="fa fa-trash fa-2x pull-right"
+            (click)="deleteHandler(u)"
+          ></i>
+        </div>
       </div>
     </div>
   `,
@@ -41,7 +72,6 @@ import { User } from './model/user';
   ],
 })
 export class AppComponent {
-  title = 'demo1';
   users: User[] = [
     // list of users
     { id: 1, label: 'Fabio', gender: 'M', age: 20 },
@@ -50,5 +80,11 @@ export class AppComponent {
   ];
   deleteHandler(userToRemove: User) {
     this.users = this.users.filter((u) => u.id !== userToRemove.id);
+  }
+  saveHandler(f: NgForm) {
+    const user = f.value as User;
+    user.id = Date.now(); // add a fake ID
+    this.users = [...this.users, user];
+    f.reset();
   }
 }
